732c3cd24f1d3cf7661da7c02eaa30082bce413a
Shahzaib Khan <ishahzaibkhan@gmail.com>
2026-01-08 23:10:03 +0500

    added query sort functionality

--- backend-services/fastapi/main.py
+++ backend-services/fastapi/main.py
@@ -2,23 +2,28 @@ from fastapi import FastAPI, Path, Query, HTTPException
 import json
 app = FastAPI()
 
+
 def data_loader():
     with open("patients.json", "r") as file:
         data = json.load(file)
     return data
 
+
 @app.get("/")
 def intro():
     return {"message": "Welcome to the Patient Management API!"}
+
 
 @app.get("/about")
 def about():
     return {"message": "This API provides information about patients."}
 
+
 @app.get("/view")
 def view_patients():
     patients = data_loader()
     return {"patients": patients}
+
 
 @app.get("/view/{patient_id}")
 def view_patient(patient_id: str = Path(..., description="ID of the patient", example="P001")):
@@ -27,3 +32,19 @@ def view_patient(patient_id: str = Path(..., description="ID of the patient", ex
         return data[patient_id]
     else:
         raise HTTPException(status_code=404, detail="Patient not found")
+
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description="Sort by height, weight, or bmi", example="height"), order: str = Query("ascending", description="Order of sorting: ascending or descending", example="ascending")):
+    valid_fields = ["height", "weight", "bmi"]
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail="Valid fields: height, weight, bmi")
+    
+    if order not in ["ascending", "descending"]:
+        raise HTTPException(status_code=400, detail="Valid options: ascending, descending")
+        
+    data = data_loader()
+
+    sort_order = True if order == "descending" else False
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by), reverse=sort_order)
+    return sorted_data
