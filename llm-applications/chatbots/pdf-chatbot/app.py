5bcae3222ac8496a039124bbcf12a19c2b94374a
Shahzaib Khan <ishahzaibkhan@gmail.com>
2026-01-30 22:23:30 +0500

    implement the functionality of extracting main sections using regex

--- llm-applications/chatbots/pdf-chatbot/app.py
+++ llm-applications/chatbots/pdf-chatbot/app.py
@@ -7,3 +7,28 @@
 # The goal is to understand the research article at least upto 80%.
 # The format should be upload pdf and display the output in a well structured manner.
 # No QnA format.
+
+import pymupdf
+import re
+
+# Load the PDF document
+doc = pymupdf.open("./article.pdf")
+
+text = ""
+filtered_matches = []
+counter = 1
+
+for page in doc:
+    text += page.get_text().lower()
+
+pattern = re.compile(r'\d\.\n(?:\w+(?:[-\s]\w+){0,4})\n')
+
+matches = pattern.findall(text)
+
+for match in matches:
+    if match.startswith(f"{counter}.\n"):
+        filtered_matches.append(match)
+        counter += 1
+
+for match in filtered_matches:
+    print(match)
